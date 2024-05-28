9b8ef3a7b96973ba6cdbe9dff7513548bb5d30fe
정소민 <yjk70702002@gmail.com>
2024-05-28 15:50:48 +0900

    Feat: BindingResult, FieldError, ObjectError

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,11 +1,17 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class Item {
 
     private Long id;
+
+    @NotBlank
+    @Range(min = 1000, max = 1000000)
     private String itemName;
     private Integer price;
     private Integer quantity;
